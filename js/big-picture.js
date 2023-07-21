57304a49c8c02ad2c8beb8b1e7fc25ad729f9e97
JakeBadlands88 <realsirius@gmail.com>
2023-07-21 16:48:30 +0600

    module 8 task 2

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,8 +1,24 @@
 import { openPopup } from './popup.js';
 
+const COMMENTS_GROUP_SIZE = 5;
+
+/**
+ * @type {ReturnType<renderCommentsGroup>}
+ */
+let renderNextComments;
+
 const bigPicture = document.querySelector('.big-picture');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentsItem = commentsList.querySelector('.social__comment');
+
+/**
+ * @param {MouseEvent & {target: Element}} event
+ */
+const onBigPictureClick = (event) => {
+  if (event.target.closest('.comments-loader')) {
+    renderNextComments();
+  }
+};
 
 /**
  * Функция создает комментарий
@@ -20,6 +36,27 @@ const createComment = (data) => {
 };
 
 /**
+ * Отрисовывает группу комментариев
+ * @param {Array<PictureComment>} data
+ * @returns {() => void}
+ */
+const renderCommentsGroup = (data) => {
+  const commentsLoadButton = bigPicture.querySelector('.comments-loader');
+  const commentsCounter = bigPicture.querySelector('.social__comment-count');
+  const totalCount = bigPicture.querySelector('.comments-count');
+
+  totalCount.textContent = String(data.length);
+  data = structuredClone(data);
+  commentsList.replaceChildren();
+
+  return () => {
+    commentsList.append(...data.splice(0, COMMENTS_GROUP_SIZE).map(createComment));
+    commentsLoadButton.classList.toggle('hidden', data.length === 0);
+    commentsCounter.textContent = `${Number(totalCount.textContent) - data.length} из ${totalCount.textContent} комментариев`;
+  };
+};
+
+/**
  * Функция создает большую фотографию
  * @param {Picture} data
  */
@@ -30,6 +67,10 @@ const renderBigPicture = (data) => {
   bigPicture.querySelector('.likes-count').textContent = String(data.likes);
   bigPicture.querySelector('.comments-count').textContent = String(data.comments.length);
   bigPicture.querySelector('.social__comments').replaceChildren(...data.comments.map(createComment));
+
+  renderNextComments = renderCommentsGroup(data.comments);
+  renderNextComments();
+  bigPicture.addEventListener('click', onBigPictureClick);
 
   openPopup(bigPicture);
 };
